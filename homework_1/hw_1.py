bbe21278c2363e61541cc60cbaa54478c353c8cb
dimanikodyuk <dimanikodyuk@gmail.com>
2021-06-05 17:11:41 +0300

    Учтены замечания по домашнему заданию №1

--- homework_1/hw_1.py
+++ homework_1/hw_1.py
@@ -1,6 +1,7 @@
 # Задание №1
 import time
 
+# 1-я функция для теста
 def my_func(count_v):
     print("Запуск основной функции")
     i = 0
@@ -11,132 +12,98 @@ def my_func(count_v):
         sum = sum+i*i
         if i == count_v:
             br_point = False
-        print(f'{i} \n')
+        #print(f'{i} \n')
 
     print(sum)
 
+# 2-я функция для теста
+def my_test(a, b, c):
+    res = a+b
+    time.sleep(1)
+    res = res + c
+    return res
 
-def outer(a):
+
+def outer(*args, **kwargs):
     def timer_func(f):
         print("Начало выполнения основной функции")
-        st_time = start_time = time.time()
-        f(a)
+        st_time = time.time()
+        time.sleep(1)
+        def inner(*fargs, **fkwargs):
+            f(*fargs, **fkwargs)
         end_time = ("%s second" % (time.time() - st_time))
         print("Конец выполнения основной функции")
-        print(f'Время выполнения функции: {end_time}')
+        print(f'Время выполнения функции {f}: {end_time} \n')
+        return inner
     return timer_func
 
-res = outer(50000)(my_func)
+
+res  = outer(500000,2)(my_func)
+res1 = outer(1,2,3,4,5)(my_test)
 
 
 
 #Задание №2
-# n1 = input("Укажите первое число ряда Фибоначчи: ")
-# n1 = int(n1)
-# n2 = input("Укажите второе число ряда Фибоначчи: ")
-# n2 = int(n2)
-# cou = input("Укажите номер искомого элемента ряда Фибоначчи: ")
-# cou = int(cou)
-#
-# arr = []
-# arr.append(n1)
-# arr.append(n2)
-#
-# i = 0
-# while i < cou -2:
-#     fib = n1 + n2
-#     n1 = n2
-#     n2 = fib
-#     arr.append(n2)
-#     i = i + 1
-#
-# print(arr)
-# print(f"Искомый элемент ряда Фибоначчи - {cou} , его значение: " + str(arr[cou-1]))
+
+def Fibonacci(check_num):
+    try:
+        arr = [0, 1]
+        n1 = arr[0]
+        n2 = arr[1]
+
+        i = 0
+        while i < check_num -2:
+            fib = n1 + n2
+            n1 = n2
+            n2 = fib
+            arr.append(n2)
+            i = i + 1
+
+        return arr[check_num-1]
+
+    except ValueError as err:
+        print(f'Это не число. Ошибка: {err}')
+    except Exception as err:
+        print(f'Произошла ошибка: {err}')
+
+num = 5
+res = Fibonacci(num)
+print(f"Искомый элемент ряда Фибоначчи - {num} , его значение: {res}")
+
+
 
 # Задание №3 Вариант 1
-# def get_sum(a,b,c):
-#     if a > b and a > c:
-#         if b > c:
-#             res = a + b
-#         else:
-#            res = a + c
-#     elif b > a and b > c:
-#         if a > c:
-#             res = b + a
-#         else:
-#             res = b + c
-#     elif c > a and c > b:
-#         if a > b:
-#             res = c + a
-#         else:
-#             res = c + b
-#     # Если все числа равны между собой
-#     elif a == b and b == c:
-#         res = a + b
-#     return res
-#
-# try:
-#     n1 = input("Укажите первое число: ")
-#     n1 = int(n1)
-#     n2 = input("Укажите второе число: ")
-#     n2 = int(n2)
-#     n3 = input("Укажите третье число: ")
-#     n3 = int(n3)
-#
-#     print(get_sum(n1,n2,n3))
-#
-# except ValueError as err:
-#     print(f'Это не число. Ошибка: {err}')
-# except Exception as err:
-#     print(f'Произошла ошибка: {err}')
+def get_sum(a,b,c):
+    num_list = [a,b,c]
 
+    try:
+        num, index = min((num, index) for (index, num) in enumerate(num_list))
+        num_list.pop(index)
+        return sum(num_list)
 
-# Задание №3 Вариант 2
-# try:
-#     n1 = input("Укажите первое число: ")
-#     n1 = int(n1)
-#     n2 = input("Укажите второе число: ")
-#     n2 = int(n2)
-#     n3 = input("Укажите третье число: ")
-#     n3 = int(n3)
-#
-#     arr = []
-#     # Добавляем элементы в список
-#     arr.append(n1)
-#     arr.append(n2)
-#     arr.append(n3)
-#
-#     # создаем отсортированный список
-#     sort_arr = sorted(arr)
-#
-#     # Проверяем, если все числа между собой равны, то находим сумму первых двух
-#     if arr[0] == arr[1] and arr[1] == arr[2]:
-#         sum = arr[0] + arr[1]
-#         print(f"Сума наибольших чисел: {sum}")
-#     # В другом случае находим сумму последних двух чисел отсортированных в порядке увеличения
-#     else:
-#         sum = sort_arr[1] + sort_arr[2]
-#         print(f"Сума наибольших чисел: {sum}")
-# except ValueError as err:
-#     print(f'Это не число. Ошибка: {err}')
-# except Exception as err:
-#     print(f'Произошла ошибка: {err}')
+    except ValueError as err:
+        print(f'Это не число. Ошибка: {err}')
+    except Exception as err:
+        print(f'Произошла ошибка: {err}')
+
+res = get_sum(11,5,89)
+print(f"Сума наибольших чисел равна: {res}")
 
 # Задание №4
-# condition = True
-# sum_num = 0
-# while condition:
-#
-#     line = input("Укажите ряд чисел через пробел: ")
-#     num = line.split(' ')
-#
-#     for n in num:
-#         try:
-#             if n.isnumeric() == False:
-#                 condition = False
-#             sum_num += int(n)
-#         except ValueError as err:
-#             print(f'Введено не число или спец. символ, заканчиваем работу программы: {err}')
-#         except Exception as err:
-#             print(f'Произошла ошибка: {err}')
-#     print(f"Сумма введенных чисел :{sum_num}")
+condition = True
+sum_num = 0
+while condition:
+
+    line = input("Укажите ряд чисел через пробел: ")
+    num = line.split(' ')
+
+    for n in num:
+        try:
+            if n.isnumeric() == False:
+                condition = False
+            sum_num += int(n)
+        except ValueError as err:
+            print(f'Введено не число или спец. символ, заканчиваем работу программы: {err}')
+        except Exception as err:
+            print(f'Произошла ошибка: {err}')
+    print(f"Сумма введенных чисел :{sum_num}")
