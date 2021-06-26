f3f14ce782d398e8fcc1b1614d8138a03a3f5d10
dimanikodyuk <dimanikodyuk@gmail.com>
2021-06-26 21:07:18 +0300

    Выполненое домашнее 11

---
+++ homework_11/hw_11.py
@@ -0,0 +1,27 @@
+# ДЛЯ СЕБЯ
+# from flask import Flask, request, render_template, Response
+#
+# import json
+#
+# app = Flask(__name__, template_folder='templates')
+#
+# @app.route("/ping")
+# def ping():
+#     return {
+#         "status": 1,
+#         "result": "OK"
+#     }
+#
+# @app.route("/")
+# @app.route("/main", methods=["GET"])
+# def homepage():
+#     return render_template("index.html", user_data=my_name)
+#
+# @app.route("/create_test_data", methods=["POST"])
+# def create_data():
+#     return "OK"
+#
+#
+# app.run(debug=True)
+
+
